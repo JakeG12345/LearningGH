0225b7eb15780d96656020e937171da1919164e0
Jake Glick <jake@glickpeople.com>
2021-04-05 06:37:23 +1000

    blue

--- lesson 2 attempt 3/lesson 2 attempt 3/ContentView.swift
+++ lesson 2 attempt 3/lesson 2 attempt 3/ContentView.swift
@@ -10,8 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
-            .font(.largeTitle)
+            .foregroundColor(Color.blue)
     }
 }
 
